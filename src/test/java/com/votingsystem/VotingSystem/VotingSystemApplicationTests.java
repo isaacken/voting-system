5c72c12510aa158afb332400fe3660701e3f5821
Isaac <isaac.kennedy@compasso.com.br>
2022-06-26 05:42:25 -0300

    Initial commit

---
+++ src/test/java/com/votingsystem/VotingSystem/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votingsystem.VotingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
