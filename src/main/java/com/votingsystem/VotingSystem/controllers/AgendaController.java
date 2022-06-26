38b55a00195de1a9a92cb9eab9a314e5ca735e54
Isaac <isaac.kennedy@compasso.com.br>
2022-06-26 06:37:28 -0300

    feat: create controller for agenda creation

---
+++ src/main/java/com/votingsystem/VotingSystem/controllers/AgendaController.java
@@ -0,0 +1,35 @@
+package com.votingsystem.VotingSystem.controllers;
+
+import com.votingsystem.VotingSystem.entities.Agenda;
+import com.votingsystem.VotingSystem.interfaces.IAgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/agenda")
+public class AgendaController {
+    private IAgendaService agendaService;
+
+    @Autowired
+    public AgendaController(IAgendaService agendaService) {
+        this.agendaService = agendaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> helloAgenda() {
+        return ResponseEntity.status(HttpStatus.OK).body("Hello Agenda");
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createAgenda(@RequestBody Agenda agenda) {
+        var createdAgenda = agendaService.create(agenda);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAgenda);
+    }
+}
