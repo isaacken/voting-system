fbab2857a5e74c1f93bd1c60fd0e42a37c44fd45
Isaac <isaac.kennedy@compasso.com.br>
2022-06-28 01:25:04 -0300

    refactor: remove unused imports

--- src/test/java/com/votingsystem/VotingSystem/controllers/AgendaControllerTest.java
+++ src/test/java/com/votingsystem/VotingSystem/controllers/AgendaControllerTest.java
@@ -1,12 +1,8 @@
 package com.votingsystem.VotingSystem.controllers;
 
 import com.votingsystem.VotingSystem.entities.Agenda;
-import com.votingsystem.VotingSystem.interfaces.IAgendaRepository;
 import com.votingsystem.VotingSystem.interfaces.IAgendaService;
 import com.votingsystem.VotingSystem.requests.CreateAgendaRequest;
-import com.votingsystem.VotingSystem.services.AgendaService;
-import org.assertj.core.api.Assert;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,19 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
 class AgendaControllerTest {
     @Mock
@@ -34,9 +24,6 @@ class AgendaControllerTest {
 
     @InjectMocks
     private AgendaController agendaController;
-
-    @Autowired
-    private MockMvc mockMvc;
 
     private Agenda agenda;
 
